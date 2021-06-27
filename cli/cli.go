5ec16359ddd9d41725abaa4810abf4e1eeab6e80
AntonStolov <Stoilovskyi.anton@gmail.com>
2021-06-27 16:06:21 +0200

    init script

---
+++ cli/cli.go
@@ -0,0 +1,32 @@
+package Cli
+
+import (
+	"flag"
+	// "fmt"
+	"log"
+	"os"
+)
+
+type Params struct {
+    File string
+    Script  string
+}
+
+func Cli() Params {
+	var paramiters Params
+    textPtr := flag.String("file", "", "File to parse. (Required)")
+    metricPtr := flag.String("script", "input", "JavaScript snippet. {input} is always processing line. Example: \"String(input).split(\"t\")[0];\"")
+    flag.Parse()
+
+    if *textPtr == "" {
+        flag.PrintDefaults()
+        os.Exit(1)
+    }
+
+    log.Printf("\nfile: %s\nscript: %s\n", *textPtr, *metricPtr)
+	
+	paramiters.File = *textPtr
+	paramiters.Script = *metricPtr
+
+	return paramiters
+}
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/robertkrimen/otto"
+	"line-editor/Cli"
+
+)
+
+func main() {
+
+	paramiters := Cli.Cli()
+	
+	file, err := os.Open(paramiters.File)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+		manipulator(paramiters.Script, scanner.Text())
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+	
+}
+
+func manipulator(jsCommand string, input string) {
+	script := `
+		var value = (function() {
+			var input = "` + input + `";
+			` +
+			`return ` + jsCommand +
+		`
+		})();
+	`
+	// fmt.Printf("full processing script: %s\n", script)
+	// fmt.Println(script)
+	vm := otto.New()
+	vm.Run(script)
+
+	if value, err := vm.Get("value"); err == nil {
+		if value_int, err := value.ToString(); err == nil {
+			fmt.Println(value_int)
+		}
+	}
+}
